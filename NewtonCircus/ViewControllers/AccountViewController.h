bdce21985092dc8d25a49f80b4f797f654c6aba1
Weitian Chan <chanweitian@gmail.com>
2014-02-12 17:07:57 +0800

    Static table done

--- NewtonCircus/ViewControllers/AccountViewController.h
+++ NewtonCircus/ViewControllers/AccountViewController.h
@@ -2,12 +2,26 @@
 //  AccountViewController.h
 //  NewtonCircus
 //
-//  Created by Weitian Chan on 11/2/14.
+//  Created by Weitian Chan on 12/2/14.
 //  Copyright (c) 2014 Weitian Chan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AccountViewController : UIViewController
+@interface AccountViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *mobileL;
+@property (strong, nonatomic) IBOutlet UILabel *firstNameL;
+@property (strong, nonatomic) IBOutlet UILabel *lastNameL;
+
+@property (strong, nonatomic) IBOutlet UILabel *accountTypeL;
+@property (strong, nonatomic) IBOutlet UILabel *accountIdL;
+@property (strong, nonatomic) IBOutlet UILabel *pendingPaymentL;
+@property (strong, nonatomic) IBOutlet UILabel *lifetimeEarningL;
+
+@property (strong, nonatomic) IBOutlet UILabel *homeL;
+@property (strong, nonatomic) IBOutlet UILabel *workL;
+@property (strong, nonatomic) IBOutlet UILabel *othersL;
+
 
 @end
--- NewtonCircus/ViewControllers/AccountViewController.m
+++ NewtonCircus/ViewControllers/AccountViewController.m
@@ -2,7 +2,7 @@
 //  AccountViewController.m
 //  NewtonCircus
 //
-//  Created by Weitian Chan on 11/2/14.
+//  Created by Weitian Chan on 12/2/14.
 //  Copyright (c) 2014 Weitian Chan. All rights reserved.
 //
 
@@ -10,13 +10,14 @@
 
 @interface AccountViewController ()
 
+
 @end
 
 @implementation AccountViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:style];
     if (self) {
         // Custom initialization
     }
@@ -26,7 +27,51 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    //UILocalizedIndexedCollation *theCollation = [UILocalizedIndexedCollation currentCollation];
+   
+    /*
+    
+    self.accountInfo = [[NSMutableArray alloc] initWithCapacity:3];
+
+    NSDictionary *accountDetails = @{
+                                     @"Mobile Number": [NSNumber numberWithInt:92387434],
+                                     @"First name": @"null",
+                                     @"Sceond name":@"null"
+                                     };
+    NSDictionary *yourEarnings = @{@"Account Type": @"ez-link",
+                                   @"Account ID":@"2334203495809385",
+                                   @"Pending Payment": [NSNumber numberWithDouble:0.00],
+                                   @"Lifetime Earning": [NSNumber numberWithDouble:0.00]
+                                   };
+    
+    
+    NSDictionary *favouriteLocation = @{@"Home": @"Amber Garden",
+                                        @"Work": @"Orchard",
+                                        @"Others": @"null"
+                                        };
+    
+    
+    
+    
+    [self.accountInfo addObject:accountDetails];
+    [self.accountInfo addObject:yourEarnings];
+    [self.accountInfo addObject:favouriteLocation];
+     
+     */
+    
+    
+    self.navigationController.navigationBar.translucent = NO;
+
+
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +79,58 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
 
 @end
